0765a574f15cce548dae6e2d70ee11087debee2b
Jackson <jacksonjunioxfranco@gmail.com>
2026-08-11 20:28:23 -0300

    Primeiro Commit

--- Aula2_Cei_Web/JS/Prog1.js
+++ Aula2_Cei_Web/JS/Prog1.js
@@ -1,2 +1,16 @@
 var valor; 
 valor=prompt('Digite um valor e Tecle enter');
+
+
+/* ------------------ Estrutura de Decisão ------------------
+if(valor % 2 == 0){
+    document.write('Valor atual é par '+valor);
+} else {
+    document.write('o valor atual é impar '+valor);
+}
+document.write(' O valor Atual é '+valor);
+------------------------------------------------------------- */
+
+for (cont=1; cont<=valor; cont++){
+    document.write(' <br> o valor atual é '+cont);
+}
